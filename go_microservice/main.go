9903f9928e4d1f80316861dfeccf267873f8f221
Nolatrae <105476991+Nolatrae@users.noreply.github.com>
2023-06-26 23:27:12 +0800

    fix: Исправление логов

--- go_microservice/main.go
+++ go_microservice/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"context"
 	"database/sql"
+	"flag"
 	"fmt"
-	cl "go_microservice/pkg/client"
 	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-	"google.golang.org/grpc"
 )
 
 type MyObject struct {
@@ -49,21 +46,28 @@ func index(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/index.html", "templates/header.html", "templates/footer.html")
 
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		// fmt.Fprintf(w, err.Error())
+		// log.Println(w, err.Error())
+		log.Println("Error", err.Error())
+
 	}
 
 	// Connect to DB
 	db, err := sql.Open("mysql", "EGOR:EGOR@tcp(127.0.0.1:3305)/calendar")
 	if err != nil {
-		fmt.Println(err.Error())
+		// fmt.Println(err.Error())
+		log.Println("Error", err.Error())
 		panic(err)
+	} else {
+		log.Println("Info", "ДБ ОК")
 	}
 	defer db.Close()
 
 	// Выборка данных
 	res, err := db.Query("Select * from `states`")
 	if err != nil {
-		fmt.Println(err.Error())
+		// fmt.Println(err.Error())
+		log.Println("Error", err.Error())
 		panic(err)
 	}
 
@@ -73,7 +77,8 @@ func index(w http.ResponseWriter, r *http.Request) {
 		var post State
 		err = res.Scan(&post.Id, &post.Title, &post.Reading, &post.Water, &post.Mood)
 		if err != nil {
-			fmt.Println(err.Error())
+			// fmt.Println(err.Error())
+			log.Println("Error", err.Error())
 			panic(err)
 		}
 		posts = append(posts, post)
@@ -97,8 +102,11 @@ func saveArticle(w http.ResponseWriter, r *http.Request) {
 		// Connect to DB
 		db, err := sql.Open("mysql", "EGOR:EGOR@tcp(127.0.0.1:3305)/calendar")
 		if err != nil {
-			fmt.Println(err.Error())
+			// fmt.Println(err.Error())
+			log.Println("Error", err.Error())
 			panic(err)
+		} else {
+			log.Println("Info", "ДБ ОК")
 		}
 		defer db.Close()
 
@@ -108,6 +116,8 @@ func saveArticle(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			fmt.Println(err.Error())
 			panic(err)
+		} else {
+			log.Println("Info", "Внесли новую запись в БД")
 		}
 		defer insert.Close()
 
@@ -122,13 +132,16 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 	t, err := template.ParseFiles("templates/show.html", "templates/header.html", "templates/footer.html")
 
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
+		// fmt.Fprintf(w, err.Error())
+		log.Println("Error", err.Error())
 	}
 
 	// Connect to DB
 	db, err := sql.Open("mysql", "EGOR:EGOR@tcp(127.0.0.1:3305)/calendar")
 	if err != nil {
-		fmt.Println(err.Error())
+		// fmt.Println(err.Error())
+		// fmt.Fprintf(w, err.Error())
+		log.Println("Error", err.Error())
 		panic(err)
 	}
 	defer db.Close()
@@ -136,7 +149,8 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 	// Выборка данных
 	res, err := db.Query(fmt.Sprintf("Select * From `states` WHERE `id` = '%s'", vars["id"]))
 	if err != nil {
-		fmt.Println(err.Error())
+		// fmt.Println(err.Error())
+		log.Println("Error", err.Error())
 		panic(err)
 	}
 
@@ -145,7 +159,8 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 		var post State
 		err = res.Scan(&post.Id, &post.Title, &post.Reading, &post.Water, &post.Mood)
 		if err != nil {
-			fmt.Println(err.Error())
+			// fmt.Println(err.Error())
+			log.Println("Error", err.Error())
 			panic(err)
 		}
 		showPost = post
@@ -156,6 +171,11 @@ func show_post(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleFunc() {
+	addr := flag.String("addr", ":8080", "Сетевой адрес веб-сервера")
+	flag.Parse()
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	rtr := mux.NewRouter()
 	rtr.HandleFunc("/", index).Methods("GET")
 	rtr.HandleFunc("/save_article", saveArticle).Methods("POST")
@@ -164,34 +184,39 @@ func handleFunc() {
 	http.Handle("/", rtr)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
 
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(*addr, nil)
+
+	// Применяем созданные логгеры к нашему приложению.
+	infoLog.Printf("Запуск сервера на %s", *addr)
+	err := http.ListenAndServe(*addr, rtr)
+	errorLog.Fatalf("Этот хост %s уже занят. Ошибка - %s ", *addr, err)
 }
 
 func main() {
 
-	fileContent, err := ioutil.ReadFile("./test_text/test5.txt")
-	if err != nil {
-		fmt.Println("Ошибка чтения файла:", err)
-		return
-	}
+	// fileContent, err := ioutil.ReadFile("./test_text/test5.txt")
+	// if err != nil {
+	// 	fmt.Println("Ошибка чтения файла:", err)
+	// 	return
+	// }
 
-	conn, err := grpc.Dial("51.250.8.139:1111", grpc.WithInsecure())
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := cl.NewTextAnalysServiceClient(conn)
-	fmt.Println(time.Now().String())
+	// conn, err := grpc.Dial("10.128.0.13:1111", grpc.WithInsecure())
+	// if err != nil {
+	// 	log.Fatalf("did not connect: %v", err)
+	// }
+	// defer conn.Close()
+	// c := cl.NewTextAnalysServiceClient(conn)
+	// fmt.Println(time.Now().String())
 
-	result, err := c.GetResult(context.Background(), &cl.SettingsTextPB{
-		Text: string(fileContent),
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(time.Now().String())
-	fmt.Println(result.GetHardReading())
-	fmt.Println(result.GetWaterValue())
-	fmt.Println(result.GetMood())
+	// result, err := c.GetResult(context.Background(), &cl.SettingsTextPB{
+	// 	Text: string(fileContent),
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(time.Now().String())
+	// fmt.Println(result.GetHardReading())
+	// fmt.Println(result.GetWaterValue())
+	// fmt.Println(result.GetMood())
 	handleFunc()
 }
