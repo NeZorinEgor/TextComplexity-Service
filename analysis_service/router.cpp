0a102466be5d79e5179abeb1d3d6652ce9a67d32
Артем Быстров <devspb5@skbpribor.ru>
2023-06-29 05:54:59 -0700

    feat: hash functions

--- analysis_service/router.cpp
+++ analysis_service/router.cpp
@@ -2,18 +2,38 @@
 #include "analys_alg.h"
 #include "db.h"
 
+static int32_t crc32(size_t len, const void* data, const unsigned int POLY = 0x04C11DB7) {
+    const unsigned char* buffer = (const unsigned char*)data;
+    unsigned int crc = -1;
+
+    while (len--)
+    {
+        crc = crc ^ (*buffer++ << 24);
+        for (int bit = 0; bit < 8; bit++)
+        {
+            if (crc & (1L << 31)) crc = (crc << 1) ^ POLY;
+            else                  crc = (crc << 1);
+        }
+    }
+    uint32_t crc_u = ~crc;
+    int32_t crc_out = *reinterpret_cast<int*>(&crc);
+
+    return std::abs(crc_out);
+}
+
 ::grpc::Status RouterService::getResult(::grpc::ServerContext *context,
                                         const ::Text_analys::SettingsTextPB *request,
                                         ::Text_analys::ResultParsingPB *response)
 {
     Result result;
 //work with hashes
-    auto opt = AppDB::Instanse().getResult(0); //TODO CRC32 
+    int32_t hash = crc32(request->text().size(), request->text().c_str());
+    auto opt = AppDB::Instanse().getResult(hash); //TODO CRC32 
     if(opt.has_value())
         result = opt.value();
     else {
         result = aal::analys(request->text()); 
-        AppDB::Instanse().addResult(0,result); //TODO CRC32
+        AppDB::Instanse().addResult(hash,result); //TODO CRC32
     }
 //set settings
     response->set_hard_reading(result.hard);
